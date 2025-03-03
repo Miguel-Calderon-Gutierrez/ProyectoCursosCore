d840b1f0de655ca9791ff04a40d046db908d902f
Miguel Calderon <miguelcalderon.dev@gmail.com>
2025-03-03 06:46:55 -0500

    Creacion de Middleware para manejo de errores

---
+++ Aplicacion/ManejadorError/ManejadorExepcion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace Aplicacion.ManejadorError
+{
+    public class ManejadorExepcion : Exception
+    {
+        public HttpStatusCode Codigo { get; }
+        public Object Errores { get; }
+
+        public ManejadorExepcion(HttpStatusCode codigo, object errores = null)
+        {
+            Codigo = codigo;
+            Errores = errores;
+        }
+    }
+}
